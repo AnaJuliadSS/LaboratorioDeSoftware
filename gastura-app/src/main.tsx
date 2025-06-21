cf487881889f706d992763a7aba5a84726124e40
AnaJuliadSS <anajuliamama@hotmail.com>
2025-06-21 17:24:34 -0300

    edit: mudando nome das rotas

--- gastura-app/src/main.tsx
+++ gastura-app/src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "../src/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute.tsx";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import Login from "./components/login/Login.tsx";
 import Cadastro from "./components/cadastro/Cadastro.tsx";
@@ -14,12 +12,12 @@ createRoot(document.getElementById("root")!).render(
 		<ChakraProvider value={defaultSystem}>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Login />} />
+					<Route path="/" element={<GasturaApp />} />
 					<Route path="/register" element={<Cadastro />} />
 					{/* <Route element={<PrivateRoute />}>
 						<Route path="/home" element={<App />} />
 					</Route> */}
-					<Route path="/home" element={<GasturaApp />} />
+					<Route path="/login" element={<Login />} />
 				</Routes>
 			</BrowserRouter>
 		</ChakraProvider>
