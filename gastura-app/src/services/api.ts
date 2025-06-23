718aa1f424e05a08e5beb5778559646b0ea792b8
AnaJuliadSS <anajuliamama@hotmail.com>
2025-06-23 19:26:41 -0300

    chamada da api para .env

--- gastura-app/src/services/api.ts
+++ gastura-app/src/services/api.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: 'https://localhost:7218/api/'
+  baseURL: apiUrl
 });
+
+
+// import axios from 'axios';
+
+// export const api = axios.create({
+//   baseURL: 'https://localhost:7218/api/'
+// });
